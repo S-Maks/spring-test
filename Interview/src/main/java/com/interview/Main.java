aa866e2591c579ff5dd4348af5921893c335978f
titok@lansoft.by <titok@lansoft.by>
2022-03-22 18:12:00 +0300

    add task for junior java developer

---
+++ Interview/src/main/java/com/interview/Main.java
@@ -0,0 +1,14 @@
+package com.interview;
+
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //Дан лист слов
+    List<String> employees = List.of("Чайник", "Король", "Отелье", "Мимика", "Венера",
+        "Танбур", "Астрал", "Аукцион");
+
+    //Вывести все слова, начинающиеся на букву "А" в верхнем регистре, с сортировкой Я-А
+  }
+}
